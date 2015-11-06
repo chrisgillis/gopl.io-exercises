1aecc9a75d1682fca4032c65aa7bfcd176560a3e
Chris Gillis <chris@stitchlabs.com>
2015-11-06 00:04:00 -0800

    3.10 and 3.11

---
+++ ch3/ex3_11/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "bytes"
+)
+
+func main() {
+    fmt.Println(comma("1230000"))
+}
+
+func comma(s string) string {
+    var buf bytes.Buffer
+
+    parts := strings.Split(s, ".")
+
+    pos := 0
+
+    if len(parts[0]) % 3 != 0 {
+        pos += len(parts[0]) %3
+        buf.WriteString(parts[0][:pos])
+        buf.Write([]byte{','})
+    }
+
+    for ; pos < len(parts[0]); pos +=3 {
+        buf.WriteString(parts[0][pos:pos+3])
+        buf.Write([]byte{','})
+    }
+    buf.Truncate(buf.Len() -1)
+
+    if len(parts) > 1 {
+        buf.Write([]byte{'.'})
+        buf.WriteString(parts[1])
+    }
+    return buf.String()
+}
